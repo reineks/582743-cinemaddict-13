02ff01a66b529d81c0077638d21afbad11b3d95d
reineks <oringarosa@mail.ru>
2020-11-26 21:22:32 +0300

    изменены названия файлов, добавлена сортировка отдельным модулем, убран лишний префикс

--- src/main.js
+++ src/main.js
@@ -2,43 +2,50 @@ import {createHeaderProfileTemplate} from "./view/header-profile.js";
 import {createMainMenuTemplate} from "./view/main-menu.js";
 import {createFilmCardTemplate} from "./view/film-card.js";
 import {createFilmDetailsTemplate} from "./view/film-details.js";
-import {createFooterFilmsStatisticsTemplate} from "./view/films-count.js";
+import {createFilmsStatisticTemplate} from "./view/films-statistic.js";
 import {createFilmsListTemplate} from "./view/films-list.js";
 import {createShowMoreButtonTemplate} from "./view/show-more-button.js";
+import {createSortTemplate} from "./view/sort.js";
 
 const FILM_COUNT = 5;
-const MOST_COMMENTED_FILMS = 2;
-const TOP_RATED_FILMS = 2;
+const MOST_COMMENTED_COUNT = 2;
+const TOP_RATED_COUNT = 2;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const siteHeaderElement = document.querySelector(`.header`);
-const siteMainElement = document.querySelector(`.main`);
-const siteBodyElement = document.querySelector(`body`);
-const siteFooterFilmsStatistics = document.querySelector(`.footer__statistics`);
+const HeaderElement = document.querySelector(`.header`);
+const MainElement = document.querySelector(`.main`);
+const BodyElement = document.querySelector(`body`);
+const FooterFilmsStatisticsElement = document.querySelector(`.footer__statistics`);
 
-render(siteHeaderElement, createHeaderProfileTemplate(), `beforeend`);
-render(siteMainElement, createMainMenuTemplate(), `beforeend`);
-render(siteMainElement, createFilmsListTemplate(), `beforeend`);
-render(siteFooterFilmsStatistics, createFooterFilmsStatisticsTemplate(), `beforeend`);
+render(HeaderElement, createHeaderProfileTemplate(), `beforeend`);
+render(MainElement, createMainMenuTemplate(), `beforeend`);
+render(MainElement, createSortTemplate(), `beforeend`);
+render(MainElement, createFilmsListTemplate(), `beforeend`);
+render(FooterFilmsStatisticsElement, createFilmsStatisticTemplate(), `beforeend`);
+render(BodyElement, createFilmDetailsTemplate(), `beforeEnd`);
 
 
-const filmsList = siteMainElement.querySelector(`.films-list > .films-list__container`);
-const filmsListRated = siteMainElement.querySelector(`.films-list + .films-list--extra .films-list__container`);
-const filmsListCommented = siteMainElement.querySelector(`.films-list--extra + .films-list--extra .films-list__container`);
+const filmsListElement = MainElement.querySelector(`.films-list > .films-list__container`);
+const filmsListRatedElement = MainElement.querySelector(`.films-list + .films-list--extra .films-list__container`);
+const filmsListCommentedElement = MainElement.querySelector(`.films-list--extra + .films-list--extra .films-list__container`);
 
-render(filmsList, createShowMoreButtonTemplate(), `afterend`);
+render(filmsListElement, createShowMoreButtonTemplate(), `afterend`);
 
 for (let i = 0; i < FILM_COUNT; i++) {
-  render(filmsList, createFilmCardTemplate(), `beforeend`);
+  render(filmsListElement, createFilmCardTemplate(), `beforeend`);
 }
 
-for (let i = 0; i < TOP_RATED_FILMS; i++) {
-  render(filmsListRated, createFilmCardTemplate(), `beforeend`);
+for (let i = 0; i < TOP_RATED_COUNT; i++) {
+  render(filmsListRatedElement, createFilmCardTemplate(), `beforeend`);
 }
 
-for (let i = 0; i < MOST_COMMENTED_FILMS; i++) {
-  render(filmsListCommented, createFilmCardTemplate(), `beforeend`);
+for (let i = 0; i < MOST_COMMENTED_COUNT; i++) {
+  render(filmsListCommentedElement, createFilmCardTemplate(), `beforeend`);
 };
+
+const Popup = BodyElement.querySelector(`.film-details`);
+Popup.classList.add(`visually-hidden`);
+
--- src/view/main-menu.js
+++ src/view/main-menu.js
@@ -8,10 +8,4 @@ export const createMainMenuTemplate = () => {
     </div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
   </nav>
-
-  <ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
-  </ul>`;
 };
